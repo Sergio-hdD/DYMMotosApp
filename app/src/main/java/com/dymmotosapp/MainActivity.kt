9aac5b1611bccc082d43df0d8e861073fefaceb4
Sergio-hdD <sergio-hdd@hotmail.com>
2026-01-02 18:04:07 -0300

    main => Recibir la imagen en el código (en MainActivity.kt) y mostrar la imagen (UI mínima, en activity_main.xml)

--- app/src/main/java/com/dymmotosapp/MainActivity.kt
+++ app/src/main/java/com/dymmotosapp/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.dymmotosapp
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+
+        if (intent?.action == android.content.Intent.ACTION_SEND) {
+            val imageUri: Uri? = intent.getParcelableExtra(android.content.Intent.EXTRA_STREAM)
+
+            if (imageUri != null) {
+                imageView.setImageURI(imageUri)
+            }
         }
     }
 }
