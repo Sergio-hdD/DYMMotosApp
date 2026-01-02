0e785c7f45d058c1b184fc18af0be76ec55f1a45
Sergio-hdD <sergio-hdd@hotmail.com>
2026-01-02 18:17:04 -0300

    main => Aplicando mejores prácticas

--- app/src/main/java/com/dymmotosapp/MainActivity.kt
+++ app/src/main/java/com/dymmotosapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.dymmotosapp
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
@@ -7,14 +8,27 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imageView = findViewById<ImageView>(R.id.imageView)
+        imageView = findViewById(R.id.imageView)
 
-        if (intent?.action == android.content.Intent.ACTION_SEND) {
-            val imageUri: Uri? = intent.getParcelableExtra(android.content.Intent.EXTRA_STREAM)
+        handleIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        if (intent != null) {
+            handleIntent(intent)
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (intent.action == Intent.ACTION_SEND && intent.type?.startsWith("image/") == true) {
+            val imageUri: Uri? = intent.getParcelableExtra(Intent.EXTRA_STREAM)
 
             if (imageUri != null) {
                 imageView.setImageURI(imageUri)
